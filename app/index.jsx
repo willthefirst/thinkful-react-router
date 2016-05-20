f1afd5bfb10f54682390fb16bcc0b418ce75a2ca
Will Moritz <will@crowdfavorite.com>
2016-05-20 09:31:33 -0400

    finished first exercise

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,95 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var router = require('react-router');
+var Router = router.Router;
+var Route = router.Route;
+var hashHistory = router.hashHistory;
+var Link = router.Link;
 
-var App = React.createClass({
-  render: function() {
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(routes, document.getElementById('app'));
+});
+
+var CONTACTS = {
+    0: {
+        id: 0,
+        name: 'Sarah Hughes',
+        phoneNumber: '01234 567890'
+    },
+    1: {
+        id: 1,
+        name: 'Tim Taylor',
+        phoneNumber: '02345 678901'
+    },
+    2: {
+        id: 2,
+        name: 'Sam Smith',
+        phoneNumber: '03456 789012'
+    }
+};
+
+var Contact = function(props) {
     return (
-      <div>I'm working!</div>
-    )
-  }
-})
+        <div>
+            <strong>
+                <Link to={'/contacts/' + props.id}>
+                    {props.name}
+                </Link>
+            </strong>
+            &nbsp;
+            {props.phoneNumber}
+        </div>
+    );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'));
+var ContactList = function(props) {
+    var contacts = Object.keys(props.contacts).map(function(contactId, index) {
+        var contact = props.contacts[contactId];
+        return (
+            <li key={index}>
+                <Contact id={contact.id} name={contact.name}
+                         phoneNumber={contact.phoneNumber} />
+            </li>
+        );
+    });
+    return (
+        <ul>
+            {contacts}
+        </ul>
+    );
+};
+
+
+var ContactListContainer = function() {
+    return <ContactList contacts={CONTACTS} />;
+};
+var IndexRoute = router.IndexRoute;
+
+var App = function(props) {
+    return (
+        <div>
+            <h1>
+                Contacts App
+            </h1>
+            <div>
+                {props.children}
+            </div>
+        </div>
+    );
+};
+
+var ContactContainer = function(props) {
+    console.log(props);
+    var contact = CONTACTS[props.params.contactId];
+    return <Contact id={contact.id} name={contact.name}
+                    phoneNumber={contact.phoneNumber} />;
+};
+
+var routes = (
+    <Router history={hashHistory}>
+        <Route path="/contacts" component={App}>
+            <IndexRoute component={ContactListContainer} />
+            <Route path=":contactId" component={ContactContainer} />
+        </Route>
+    </Router>
+);
